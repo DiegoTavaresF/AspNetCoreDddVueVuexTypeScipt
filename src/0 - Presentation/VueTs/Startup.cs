0de6e70271093659804417c520ad3d88a539007f
diegotavaresferreira2@gmail.com <diegotavaresferreira2@gmail.com>
2020-03-20 19:17:23 +0000

    fix error

--- src/0 - Presentation/VueTs/Startup.cs
+++ src/0 - Presentation/VueTs/Startup.cs
@@ -24,7 +24,6 @@ namespace VueTs
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -55,7 +54,6 @@ namespace VueTs
             });
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
